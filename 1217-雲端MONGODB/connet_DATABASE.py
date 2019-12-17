5260f695e8d608e6a9f36872c96ffa61e54b2a52
annie784095 <57478985+annie784095@users.noreply.github.com>
2019-12-17 19:34:16 +0800

    雲端MONGO
    雲端MONGO

---
+++ 1217-雲端MONGODB/connet_DATABASE.py
@@ -0,0 +1,17 @@
+﻿import pymongo
+
+my_client= pymongo.MongoClient("mongodb+srv://maomao:maomao123@animal-axwfm.gcp.mongodb.net/user_info?retryWrites=true&w=majority")
+
+
+my_database = my_client.user_info
+
+my_collection = my_database.neko
+my_collection.insert_one({
+    "name": "apple",
+    "calories": 266,
+    "fats": {
+        "saturated": 4.5,
+        "trans": 0.2
+    },
+    "protein": 11
+})
