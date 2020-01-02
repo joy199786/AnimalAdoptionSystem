6a561035571731e6d1c30bf465a791d50278c4f9
LU Shu Xian <57816306+rabby56789@users.noreply.github.com>
2020-01-02 19:26:41 +0800

    UI update
    UI UI

--- agency_adoption.php
+++ agency_adoption.php
@@ -3,9 +3,9 @@
 <html lang="zh-TW" dir="ltr">
 	<head>
 		<meta charset = "utf-8">
-		<title>個人認養--動物認養系統</title>
+		<title>機構認養--動物認養系統</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/person_adoption.css">
+		<link rel="stylesheet" type="text/css" href="css/agency_adoption.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js">
 		</script>
