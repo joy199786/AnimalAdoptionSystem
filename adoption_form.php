db7a0307cb36d0545bb83b5296f7af789da7be0a
Bonnie <m90322@gmail.com>
2020-01-07 17:15:19 +0800

    表單與註冊
    篩選器也有修

--- adoption_form.php
+++ adoption_form.php
@@ -5,7 +5,7 @@
 		<meta charset = "utf-8">
 		<title>申請認養頁面--動物認養系統</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/">
+		<link rel="stylesheet" type="text/css" href="css/adoption_form.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js">
 		</script>
@@ -26,12 +26,12 @@
 		</div>
 		<?php $recive=$doc['account'];
 		$url='https://formspree.io/'.$recive;
-		echo '<div class="context">
+		echo '<div class="context_left">
 			<form action="';echo $url;echo '" method="post">	
-			<div class="title">個人基本資料</div>
+			<div class="title">個人基本申請資料</div>
 			<p>姓名: <input type="text" name="user_name" required></p><br>
 			<p>出生日期: <input type="text" name="ago" required></p><br>
-			<p>性別：<input type="radio" value="男" name="gender">男<input type="radio" value="女" name="gender" checked>女
+			<p>性別：<input class="radio" type="radio" value="男" name="gender">男<input class="radio" type="radio" value="女" name="gender" checked>女
 			<p>身分證字號: <input type="text" name="IDNumber" required></p><br>
 			<p>信箱(Email): <input type="email" name="account" required></p><br>
 			<p>住址: <input type="text" name="address" required></p><br>
--- user_foster.php
+++ user_foster.php
@@ -12,45 +12,67 @@
 		
 	</head>
 	<body>
-		<div class="foster_available">
+		<div class="foster_available" id="pet_list">
 			<div class="foster_operation">
 				<div class="foster_operation_left"><h4>可供送養</h4></div>
 				<div class="foster_operation_right">
 					<button class="new" type="button" onclick="location.href='new_animal_info.php'">新增</button>
 				</div>
 			</div>
-			<div class="a_animal">
-				<a href="animal_info.php"><img src="https://asms.coa.gov.tw/Amlapp/Upload/pic/79866ec4-0259-4c05-83fd-ec4ff90893ca.jpg" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
-				<p>種類：</p>
+			<?php include 'Available_for_adoption.php'?>
+			<!--<div class="a_animal">
+				<a href="animal_info.html"><img src="https://asms.coa.gov.tw/Amlapp/Upload/pic/79866ec4-0259-4c05-83fd-ec4ff90893ca.jpg" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
 				<p>品種：</p>
-				<p>地區：</p>
-				<p>是否提供認養：</p>
 				<button type="button">修改</button>
 				<button type="button">刪除</button>
 			</div>
 			<div class="a_animal">
-				<a href="animal_info.php"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
-				<p>種類：</p>
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
 				<p>品種：</p>
-				<p>地區：</p>
-				<p>是否提供認養：</p>
 				<button type="button">修改</button>
 				<button type="button">刪除</button>
 			</div>
+			<div class="a_animal">
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
+				<p>品種：</p>
+				<button type="button">修改</button>
+				<button type="button">刪除</button>
+			</div>
+			<div class="a_animal">
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
+				<p>品種：</p>
+				<button type="button">修改</button>
+				<button type="button">刪除</button>
+			</div>
+			<div class="a_animal">
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
+				<p>品種：</p>
+				<button type="button">修改</button>
+				<button type="button">刪除</button>
+			</div>-->
 		</div>
 		<div class="foster_finish">
 			<div class="foster_operation">
 				<div class="foster_operation_left"><h4>送養成功</h4></div>
 			</div>
-			<div class="a_animal">
-				<a href="animal_info.php"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
-				<p>種類：</p>
+			<?php include 'Successful_adoption.php'?>
+			<!--<div class="a_animal">
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
 				<p>品種：</p>
-				<p>地區：</p>
-				<p>是否提供認養：</p>
 				<button type="button">回復</button>
 			</div>
+			<div class="a_animal">
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
+				<p>品種：</p>
+				<button type="button">回復</button>
+			</div>
+			<div class="a_animal">
+				<a href="animal_info.html"><img src="" alt="no image" onerror=this.src="ui_img/no_image.png"></a>
+				<p>品種：</p>
+				<button type="button">回復</button>
+			</div>-->
 		</div>
 		
 	</body>
+
 </html>
