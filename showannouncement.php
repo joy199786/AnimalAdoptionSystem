270663963abe463926f5cafa2acf73163c5b1df0
joy199786 <55444376+joy199786@users.noreply.github.com>
2020-01-08 22:35:58 +0800

    展示公告

--- showannouncement.php
+++ showannouncement.php
@@ -8,11 +8,17 @@ echo "公告:"."<br>";
 <textarea wrap="off" style="font-family:fantasy;font-weight:bold; background-color:#FFBA3B; border:3px green solid;" name="content" cols="60" rows="10"   readonly="readonly">
 <?php
 // Echo session variables that were set on previous page
-if(!isset($_COOKIE["announcement"])) {
-    echo "announcement is not set!";
-} else {
-    echo $_COOKIE["announcement"];
-}
+$manager = new MongoDB\Driver\Manager("mongodb+srv://maomao:maomao123@animal-axwfm.gcp.mongodb.net/test?retryWrites=true&w=majority");//設定連線
+		$filter = [];//查詢條件
+		$query = new MongoDB\Driver\Query($filter);//設定查詢變數
+		$cursor = $manager->executeQuery('mydb.announcement', $query);//設定指標變數:查詢變數指向哪個db哪個collection
+		//顯示資料
+		foreach ($cursor as $document) {
+			//設定$doc為陣列才能一一顯示值
+			$doc = (array)$document;
+			if(!empty($doc)){
+				echo $doc['announce'];
+			}
 ?>
 </textarea>
 </pre>
