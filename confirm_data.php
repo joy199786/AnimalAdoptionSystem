442fa38534ef7a28b7c3e691111984ec1c4de055
joy199786 <55444376+joy199786@users.noreply.github.com>
2020-01-08 19:50:28 +0800

    重複帳號

--- confirm_data.php
+++ confirm_data.php
@@ -11,6 +11,22 @@
 			echo '<script>alert("驗證碼錯誤");</script>';
 		}
 	}
+	//been registered
+	if(!empty($_POST['account'])){
+		$manager = new MongoDB\Driver\Manager("mongodb+srv://maomao:maomao123@animal-axwfm.gcp.mongodb.net/test?retryWrites=true&w=majority");//設定連線
+		$filter = ['account' => ['$eq' => $_POST['account']]];//查詢條件
+		$query = new MongoDB\Driver\Query($filter);//設定查詢變數
+		$cursor = $manager->executeQuery('mydb.Userinfo', $query);//設定指標變數:查詢變數指向哪個db哪個collection
+		//顯示資料
+		foreach ($cursor as $document) {
+			//設定$doc為陣列才能一一顯示值
+			$doc = (array)$document;
+			if(!empty($doc)){
+				echo '<script>alert("帳號已註冊");</script>';
+				header("refresh:0;url=create_account.php");
+			}
+		}
+	}
 ?>
 <html lang="zh-TW" dir="ltr">
 	<head>
