4d508ea1dd6165da274ed225b805333635209c01
LU Shu Xian <57816306+rabby56789@users.noreply.github.com>
2020-01-10 00:18:38 +0800

    終級版
    終級版

--- admin.php
+++ admin.php
@@ -5,6 +5,10 @@
 		<title>管理頁面--動物認養系統</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="css/admin.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="Shortcut Icon" type="image/x-icon" href="ui_img/cbpig-57hae-001.ico"/>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+		</script>
 	</head>
 	<body>
 		<div class="header">
@@ -25,9 +29,9 @@
 		  {
 			echo '<div class="header_right dropdown">';
 			if($_SESSION['admin']=="False")
-			{echo '<h3>hi, <a style="color:#02e88b" href="user_index.php">';print_r($_SESSION['user_name']);echo '</a></h3>';}
+			{echo '<h3>hi, <a style="color:#02e88b" href="user_index.php">';print_r($_SESSION['nName']);echo '</a></h3>';}
 			else
-			{echo '<h3>hi, <a style="color:#02e88b" href="admin.php">';print_r($_SESSION['user_name']);echo '</a></h3>';}
+			{echo '<h3>hi, <a style="color:#02e88b" href="admin.php">';print_r($_SESSION['nName']);echo '</a>管理員</h3>';}
 			echo '<div class="dropdown_content">';
 			echo '<a href="logout.php"><h3>登出</h3></a>';
 			echo '</div>';
@@ -47,19 +51,25 @@
 		      <label for="psd"><b>密碼:</b></label>
 		      <input type="password" placeholder="輸入密碼" name="psd" required>
 		      <button type="submit" >登入</button>
-			  <p>沒有帳戶嗎？點<a href="#" name="user_register.php">這裡</a>註冊新帳戶</p>
+			  <p>沒有帳戶嗎？點<a href="#" name="create_account.php">這裡</a>註冊新帳戶</p>
 		    </div>
 		  </form>
 		</div>
 		
-		<div class="context">
-			<div class="title">管理系統</div>
-			<p>hi,<?php echo $_SESSION['user_name']?>管理員</p>
-			<button type="button" onclick="location.href='index.php'">首頁</button>
-			<button type="button" onclick="location.href='new_announcement_info.php'">新增公告</button>			
-			<button type="button" onclick="location.href='admin_user.php'">會員管理</button>
-			<button type="button" onclick="location.href='admin_pet.php'">動物管理</button>
-			<button type="button"></button>
+		<div id="navbar" class="navbar">
+		  <a class="active" href="#" name="index_content.php">首頁</a>
+		  <a href="#" name="new_announcement_info.php">新增公告</a>
+		  <a href="#" name="admin_user.php">會員管理</a>
+		  <a href="#" name="admin_pet.php">動物管理</a>
+		  <a href="#" name="admin_adoption_form_View.php">紀錄查詢</a>
+		  <a href="#" name="admin_findpet.php">遺失管理</a>
+		  <a href="#" name="admin_second.php">二手管理</a>
+		  <a href="#" name="new_message_info.php">系統信件</a>
+		  <a href="#" name="showAdminMail.php">客服信件</a>
+		</div>
+
+		<div id="content">
+			<iframe src="index_content.php"></iframe>
 		</div>
 		
 		<script>
